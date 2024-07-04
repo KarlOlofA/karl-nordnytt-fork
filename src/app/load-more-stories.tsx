efaef6f86a04c8b4fe21a81a6afdf5555f630cee
KarlOlofA <karl.98oa@gmail.com>
2024-07-04 13:01:04 +0200

    Infinite Scroll & Filter Functionality
    Added functionality for scrolling to infinity!
    Also a filter functionality between Top & New stories.

---
+++ src/app/load-more-stories.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, {useEffect, useState} from "react";
+import {Story} from "@/types";
+import {useInView} from "react-intersection-observer";
+import {Spinner} from "@/app/spinner";
+import {Stories, StoryProps} from "@/app/stories";
+import {fetchStories} from "@/app/fetch-stories";
+
+export function LoadMoreStories(initStories: StoryProps){
+    let [filterText, setFilterText] = useState("Top Stories");
+    let [fetchTopStories, setFetchTopStories] = useState(true);
+    let [stories, setStories] = useState<Story[]>(initStories.stories);
+    let [pagesLoaded = 0, setPagesLoaded] = useState(0);
+    let [hasMoreStories, setHasMoreStories] = useState(initStories.hasMoreStories);
+
+    const {ref, inView} = useInView();
+
+    const delay = (ms: number | undefined) => new Promise(res => setTimeout(res, ms));
+
+    function clearStories() {
+        setStories([]);
+        setPagesLoaded(-1);
+        setHasMoreStories(true)
+    }
+
+    function filterClick(){
+        clearStories();
+        setFetchTopStories(prev => !prev);
+
+        if (fetchTopStories){
+            setFilterText("New Stories")
+            return;
+        }
+
+        setFilterText("Top Stories")
+    }
+
+    useEffect(() => {
+        if (inView){
+            loadMoreStories();
+        }
+    }, [inView]);
+
+
+    const loadMoreStories = async () => {
+        await delay(300);
+        const nextPage = pagesLoaded + 1;
+        const {stories: newStories, hasMoreStories} = await fetchStories({page: nextPage, fetchTopStories}) ?? {stories: [], hasMoreStories: false};
+        setStories((prevStories: Story[]) => [...prevStories, ...newStories]);
+        setPagesLoaded(nextPage);
+        setHasMoreStories(hasMoreStories)
+    };
+
+    return(
+        <>
+            <div>
+                <button onClick={filterClick}>Current Filter: {filterText}</button>
+            </div>
+
+            <Stories stories={stories} hasMoreStories={hasMoreStories}/>
+
+            {hasMoreStories && (
+                <div
+                    className="flex justify-center items-center p-4 col-span-1 sm:col-span-2 md:col-end-3"
+                    ref={ref}
+                >
+                    <Spinner/>
+                </div>
+            )
+            }
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
-import { getTopStories } from "@/services/hn";
-import Link from "next/link";
+import {fetchStories} from "@/app/fetch-stories";
+import {LoadMoreStories} from "@/app/load-more-stories";
+import {StoryProps} from "@/app/stories";
 
 export default async function Home() {
-  const topstories = await getTopStories();
+    const initStories: StoryProps = await fetchStories({page: 0, fetchTopStories: true});
 
-  return (
-    <main>
-      { topstories.map((story, index) => {
-        const url = story.url ? new URL(story.url) : null;
-
-        return (
-          <div className="leading-none mb-4" key={story.id}>
-            <a title={story.title} href={story.url || `/${story.id}`} target={url?.host ? "_blank" : ""} className="pb-1 whitespace-nowrap text-ellipsis overflow-hidden block font-bold visited:text-slate-500">{index+1}. {story.title}</a>
-            <div className="text-xs text-slate-700">{url?.host ? `${url?.host} - ` : ''} <Link href={`/${story.id}`}>{story.score} poäng - {story.descendants || 'Inga'} kommentarer - {Math.floor(Date.now()/1000 - story.time)} sekunder sedan</Link></div>
-          </div>
-        )
-      }) }
-    </main>
-  );
+    return (
+        <main>
+            <LoadMoreStories stories={initStories.stories} hasMoreStories={initStories.hasMoreStories}></LoadMoreStories>
+        </main>
+    );
 }
